8ba4f8828067ddfcde122a07fa1d33ec8db1005f
YuviPanda <yuvipanda@gmail.com>
2021-03-16 16:51:40 +0530

    Add initial frontend js commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+// import Form from "@rjsf/core";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from '@rjsf/bootstrap-4';
+import schema from './schema.json';
+import uiSchema from './uiSchema.json';
 
+function updateConfig(formData) {
+    const config = formData.formData;
+    const response = fetch('/services/configurator/config',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(config)
+    })
+}
 function App() {
-    const [state, setState] = useState("CLICK ME");
 
-    return <button onClick={() => setState("CLICKED")}>{state}</button>;
+    return <Form
+        schema={schema}
+        uiSchema={uiSchema}
+    onChange={console.log}
+        onSubmit={updateConfig}
+    onError={console.log} />
+
 }
 
 render(<App />, document.getElementById("root"));
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,15 +9,20 @@ module.exports = {
     ],
     module: {
         rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: ["babel-loader"]
-        }
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
         ]
     },
     entry: { index: path.resolve(__dirname, "src", "index.js") },
     output: {
-        path: path.resolve(__dirname, "build")
-    }
+        path: path.resolve(__dirname, "../jupyterhub_configurator/static")
+    },
+    watch: true
 };
