ec21c243f1fff68c35db1f0cb13b48e90d1e52aa
YuviPanda <yuvipanda@gmail.com>
2021-03-16 17:12:04 +0530

    Add a setup.py & requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyterhub-configurator',
+    version='1.0',
+    packages=find_packages(),
+    license='3-BSD',
+    author="yuvipanda",
+    author_email="yuvipanda@gmail.com",
+    install_requires=[
+        'tornado',
+    ]
+)
