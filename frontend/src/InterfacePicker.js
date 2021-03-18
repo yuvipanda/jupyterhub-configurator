99b6d862e04352d3ac32d451d576cf55a7fd2dad
YuviPanda <yuvipanda@gmail.com>
2021-03-18 16:36:49 +0530

    Move card into its own file

--- frontend/src/InterfacePicker.js
+++ frontend/src/InterfacePicker.js
@@ -1,42 +1,5 @@
 import React, { useState } from "react";
-import styles from "./InterfacePicker.css";
-import { SelectableGroup, createSelectable } from "react-selectable-fast";
-import classnames from "classnames";
-
-const Card = (props) => {
-  const { title, description, value, id, selected, onChange } = props;
-
-  const [isActive, setIsActive] = useState(false);
-
-  const classes = [
-    "custom-control custom-radio",
-    "col-4",
-    styles.option,
-    selected ? styles.selected : null,
-    isActive ? styles.active : null,
-  ];
-  return (
-    <div
-      className={classnames(classes)}
-      onMouseOver={() => setIsActive(true)}
-      onMouseLeave={() => setIsActive(false)}
-    >
-      <input
-        className="custom-control-input"
-        type="radio"
-        name={id}
-        value={value}
-        onChange={onChange}
-        checked={selected}
-        id={id + value}
-      />
-      <label className="custom-control-label" htmlFor={id + value}>
-        <h5>{title}</h5>
-        <p className="muted-text">{description}</p>
-      </label>
-    </div>
-  );
-};
+import Card from "./Card";
 
 const InterfacePicker = (props) => {
   const interfaces = props.schema.enumMetadata.interfaces;
