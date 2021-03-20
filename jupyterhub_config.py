cc27008c9aafdbfb898a6f1ec318c0d5fc043ed6
YuviPanda <yuvipanda@gmail.com>
2021-03-20 16:02:13 +0530

    Add jupyterhub_config.py file

---
+++ jupyterhub_config.py
@@ -0,0 +1,24 @@
+import sys
+import json
+import aiohttp
+from jupyterhub.spawner import SimpleLocalProcessSpawner
+from jupyterhub_configurator.mixins import ConfiguratorSpawnerMixin
+
+c.JupyterHub.authenticator_class = "dummy"
+
+c.Authenticator.admin_users = ["yuvipanda"]
+
+c.JupyterHub.services = [
+    {
+        "name": "configurator",
+        "url": "http://127.0.0.1:10101",
+        "command": [sys.executable, "-m", "jupyterhub_configurator.app"],
+    }
+]
+
+
+class CustomSpawner(ConfiguratorSpawnerMixin, SimpleLocalProcessSpawner):
+    pass
+
+
+c.JupyterHub.spawner_class = CustomSpawner
