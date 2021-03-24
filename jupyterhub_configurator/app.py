983a6e5e318ea336b760fbdfd54778b00f86be31
YuviPanda <yuvipanda@gmail.com>
2021-03-24 17:12:43 +0530

    Format app.py

--- jupyterhub_configurator/app.py
+++ jupyterhub_configurator/app.py
@@ -22,15 +22,15 @@ from jupyterhub.utils import url_path_join, auth_decorator
 from jinja2 import Environment, FileSystemLoader
 
 HERE = os.path.dirname(__file__)
-jinja_env = Environment(loader=FileSystemLoader(os.path.join(HERE, 'templates')))
+jinja_env = Environment(loader=FileSystemLoader(os.path.join(HERE, "templates")))
+
 
 class StorageBackend:
     def __init__(self):
-        self.config_path = os.path.join(os.getcwd(), 'configurator_data.json')
-
+        self.config_path = os.path.join(os.getcwd(), "configurator_data.json")
 
     def write(self, config):
-        with open(self.config_path, 'w') as f:
+        with open(self.config_path, "w") as f:
             json.dump(config, f)
 
     # FIXME: Cache this, perhaps based on stat call?
@@ -53,64 +53,61 @@ def admin_only(self):
     just JSON.
     """
     user = self.current_user
-    if user is None or user['admin'] != True:
+    if user is None or user["admin"] != True:
         raise web.HTTPError(403)
+
 
 class ConfiguratorHandler(HubOAuthenticated, web.RequestHandler):
     # FIXME: PUT THIS BEHIND AUTH AGAIN,
     # BUT ONLY WHEN THE HUB PROCESS CAN TALK TO IT AFTER
     # BEING AUTHENTICATED
     async def get(self):
-        storage_backend = self.settings['storage_backend']
-        self.set_header('content-type', 'application/json')
+        storage_backend = self.settings["storage_backend"]
+        self.set_header("content-type", "application/json")
         self.write(storage_backend.read())
 
     @authenticated
     @admin_only
     async def post(self):
-        storage_backend = self.settings['storage_backend']
+        storage_backend = self.settings["storage_backend"]
         config = json.loads(self.request.body)
         storage_backend.write(config)
+
 
 class UIHandler(HubOAuthenticated, web.RequestHandler):
     @authenticated
     @admin_only
     def get(self):
         print(self.current_user)
-        ui_template = jinja_env.get_template('index.html')
-        self.write(ui_template.render(
-            base_url=self.settings['base_url']
-        ))
+        ui_template = jinja_env.get_template("index.html")
+        self.write(ui_template.render(base_url=self.settings["base_url"]))
+
 
 def main():
-    prefix = os.environ['JUPYTERHUB_SERVICE_PREFIX']
+    prefix = os.environ["JUPYTERHUB_SERVICE_PREFIX"]
     mp = partial(url_path_join, prefix)
 
     tornado_settings = {
-        'cookie_secret': secrets.token_bytes(32),
-        'storage_backend': StorageBackend(),
-        'static_path': os.path.join(HERE, "static"),
-        'static_url_prefix': mp('static/'),
-        'base_url': prefix
+        "cookie_secret": secrets.token_bytes(32),
+        "storage_backend": StorageBackend(),
+        "static_path": os.path.join(HERE, "static"),
+        "static_url_prefix": mp("static/"),
+        "base_url": prefix,
     }
 
     handlers = [
-        (mp(r'oauth_callback'), HubOAuthCallbackHandler),
-        (mp(r'config'), ConfiguratorHandler),
-        (mp(r'/'), UIHandler)
+        (mp(r"oauth_callback"), HubOAuthCallbackHandler),
+        (mp(r"config"), ConfiguratorHandler),
+        (mp(r"/"), UIHandler),
     ]
-    app = web.Application(
-        handlers,
-        debug=True,
-        **tornado_settings
-    )
+    app = web.Application(handlers, debug=True, **tornado_settings)
 
     http_server = HTTPServer(app)
 
-    http_server.listen(10101, '0.0.0.0')
+    http_server.listen(10101, "0.0.0.0")
 
     IOLoop.current().start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
